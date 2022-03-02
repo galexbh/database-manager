ffc72d4e513b20aebb080f33af6380f833a00430
galexbh <galexbh@protonmail.com>
2022-03-02 04:53:28 -0600

    feat(db): connection
    postgresql

--- src/main/java/com/unah/usermanager/utils/PostgreSQLAdapter.java
+++ src/main/java/com/unah/usermanager/utils/PostgreSQLAdapter.java
@@ -2,12 +2,42 @@ package com.unah.usermanager.utils;
 
 import com.unah.usermanager.utils.interfaces.DBAdapter;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class PostgreSQLAdapter implements DBAdapter {
     @Override
     public Connection getConnection(){
         Connection connection = null;
+
+        try {
+            Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance();
+
+            connection = DriverManager.
+                    getConnection("jdbc:postgresql://localhost:5438/mydb?" +
+                            "user=galex&password=mssql1Ipw");
+
+            System.out.println("Conecto");
+
+        } catch (SQLException |
+                InvocationTargetException |
+                ClassNotFoundException |
+                InstantiationException |
+                IllegalAccessException |
+                NoSuchMethodException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         return connection;
     }
 }
